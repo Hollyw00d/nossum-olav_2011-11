3a831bbbdca89d20ffc29fa2395bf02db6e2effc
Matt Jennings <matt@mattjennings.net>
2014-11-12 19:57:27 -0800

    added all other files

---
+++ index.php
@@ -0,0 +1,232 @@
+<?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Video and Motion Design by Olav Nossum - Seattle WA</title>
+	<script type="text/javascript" src="mediaelement/jquery.js"></script>
+	<script type="text/javascript" src="mediaelement/mediaelement-and-player.min.js"></script>
+	<link rel="stylesheet" href="mediaelement/mediaelementplayer.min.css" />
+
+	
+    <link rel="stylesheet" href="css/coda-slider-2.0.css" type="text/css" media="screen" />
+    <!--[if IE 7]>
+    <link rel="stylesheet" href="css/coda-slider-2.0_ie7_min.css" type="text/css" media="screen" />
+    <![endif]-->
+	
+	
+	
+    <link rel="stylesheet" type="text/css" href="fancybox/jquery.fancybox-1.3.4_min.css" media="screen" />
+	
+</head>
+<body class="crossdomain">
+      	<div id="header-container">
+            <div id="header-wrapper">
+        
+            	<a id="logo">Video and Motion Design by Olav Nossum - Seattle WA</a>
+        
+            </div><!-- #header-wrapper -->
+        </div><!-- #header-container -->
+<noscript>
+	<div>
+        <p>Unfortunately your browser does not hava JavaScript capabilities which are required to exploit full functionality of our site. This could be the result of two possible scenarios:</p>
+        <ol>
+            <li>You are using an old web browser, in which case you should upgrade it to a newer version. We recommend the latest version of <a href="http://www.getfirefox.com">Firefox</a>.</li>
+            <li>You have disabled JavaScript in you browser, in which case you will have to enable it to properly use our site. <a href="http://www.google.com/support/bin/answer.py?answer=23852">Learn how to enable JavaScript</a>.</li>
+        </ol>
+    </div>
+</noscript>
+
+<div class="coda-slider-wrapper">
+	<div class="coda-slider preload" id="coda-slider-1">
+    
+		<div class="panel">
+			<div class="panel-wrapper">
+				<h1>Make your vision, visual... with video and motion design.</h1>
+                
+                <h2>Motion Design Reel</h2>
+                
+                <p>
+                
+<!--                  <iframe src="http://player.vimeo.com/video/35923285?title=0&amp;byline=0&amp;portrait=0" width="650" height="366" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+-->
+<!--
+                  <iframe src="video01-LG.php" width="680" height="800" allowtransparency="true" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+-->
+
+<video width="640" height="360" id="player2" poster="videos/reel_2011.png" controls="controls" preload="auto">
+	<!-- MP4 source must come first for iOS -->
+	<source type="video/mp4" src="videos/reel_2011.mp4" />
+	<!-- WebM for Firefox 4 and Opera -->
+	<source type="video/webm" src="videos/reel_2011.webm" />
+	<!-- OGG for Firefox 3 -->
+	<source type="video/ogg" src="videos/reel_2011.ogv" />
+	<!-- Fallback flash player for no-HTML5 browsers with JavaScript turned off -->
+	<object width="640" height="360" type="application/x-shockwave-flash" data="mediaelement/flashmediaelement.swf">
+		<param name="movie" value="mediaelement/flashmediaelement.swf" />
+		<param name="flashvars" value="controls=true&amp;file=videos/reel_2011.mp4" />
+		<!-- Image fall back for non-HTML5 browser with JavaScript turned off and no Flash player installed -->
+		<img src="videos/reel_2011.png" width="640" height="360" alt="Here we are"
+			title="No video playback capabilities" />
+	</object>
+</video>
+
+
+        
+
+
+       
+                    
+                    
+                    
+                </p>
+                
+                <p>
+                Blah blah blah more text hereblah blah blah more text hereblah blah blah more text hereblah blah blah more text hereblah blah blah more text hereblah blah blah more text hereblah blah blah more text here
+                </p>                         
+                
+			</div>
+		</div>
+		<div class="panel">
+			<div class="panel-wrapper">
+            
+				<h1>View a few of my videos and motion design works.</h1>
+                
+                <table>
+                
+                
+                	<tr>
+                    	<td>
+                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
+                     	</td>
+                        <td>
+                        <h2>Larry Bernandez Bobblehead Commercial</h2>
+                        <p>Larry Bernandez Bobblehead Commercial</p>
+                        </td>
+                    </tr>
+                	<tr>
+                    	<td>
+                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
+                     	</td>
+                        <td>
+                        <h2>Larry Bernandez Bobblehead Commercial</h2>
+                        <p>Larry Bernandez Bobblehead Commercial</p>
+                        </td>
+                    </tr>
+                	<tr>
+                    	<td>
+                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
+                     	</td>
+                        <td>
+                        <h2>Larry Bernandez Bobblehead Commercial</h2>
+                        <p>Larry Bernandez Bobblehead Commercial</p>
+                        </td>
+                    </tr>
+                	<tr>
+                    	<td>
+                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
+                     	</td>
+                        <td>
+                        <h2>Larry Bernandez Bobblehead Commercial</h2>
+                        <p>Larry Bernandez Bobblehead Commercial</p>
+                        </td>
+                    </tr>
+                	<tr>
+                    	<td>
+                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
+                     	</td>
+                        <td>
+                        <h2>Larry Bernandez Bobblehead Commercial</h2>
+                        <p>Larry Bernandez Bobblehead Commercial</p>
+                        </td>
+                    </tr>
+                    
+                   
+                </table>
+                
+                
+			</div>
+		</div>
+		<div class="panel">
+			<div class="panel-wrapper">
+            
+				<h1><span class="big-text">Hi, I’m Olav,</span> blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah. </h1>
+                
+                <p class="pic">
+                <img src="images/about/olav-at-puget-sound.png" width="682" height="258" alt="Olav at Puget Sound" />
+                </p>
+                
+				<p>                
+				I live in Seattle, Washington.  blah blah blah I live in Seattle, WA.  blah blah blah I live in Seattle, Washington.  blah blah blah I live in Seattle, Washington.  Blah blah blah.
+				</p>
+                                
+                <p class="pic">
+                <img src="images/about/seattle01.png" width="682" height="258" alt="Seattle" />
+                </p>
+                
+			</div>
+		</div>
+		<div class="panel">
+			<div class="panel-wrapper">
+				<h1>Olav Nossum</h1>
+                <h2>
+                <a href="mailto:Olav@CutMotion.com">Olav@CutMotion.com</a>
+                <!--
+                <script type="text/javascript">
+					var addressPart1 = 'Olav@cut'
+					var addressPart2 = 'Motion.c';
+					var addressPart3 = 'om';
+					var emailPart1 = '<a href="';
+					var emailPart2 = 'mailt';
+					var emailPart3 = 'o:';
+					var emailPart4 = '"';
+					var emailPart5 = '>';
+					var emailPart6 = '<';
+					var emailPart7 = '/';
+					var emailPart8 = 'a';
+					var emailPart9 = '>';
+					document.write(emailPart1 + emailPart2 + emailPart3 + addressPart1 + addressPart2 + addressPart3 + emailPart4 + emailPart5 + addressPart1 + addressPart2 + addressPart3 + emailPart6 + emailPart7 + emailPart8 + emailPart9);		
+					</script>
+                    <noscript><span class="red">
+                    If you cannot see my email address on this page you do not have JavaScript enabled.</span> <a href="http://www.alanwood.net/demos/enabling-javascript.html" target="_blank">To enable JavaScript (a common, safe programming language for web sites) and see my email address follow these steps.</a>
+                    </noscript>
+               	-->
+                </h2>
+                
+                <p class="pic">
+                <img src="images/contact/olav-at-mountains.png" width="682" height="258" alt="Olav at the Mountains" />
+                </p>
+                
+				<p>Contact me... Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, eblah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  </p>
+			</div>
+		</div>
+        
+        
+        
+	</div><!-- .coda-slider -->
+</div><!-- .coda-slider-wrapper -->
+
+        <div id="footer-container">
+            <div id="footer-wrapper">
+            
+			<?php ini_set('date.timezone','America/Los_Angeles'); ?>            
+    
+            <h2>MAKE YOUR VISION VISUAL</h2>
+            <p>&copy; <?php print date('Y'); ?> Olav Nossum.</p>
+                          
+            </div><!-- #footer-wrapper -->
+        </div><!-- #footer-container -->
+		
+	
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.coda-slider-2.0.js"></script>
+<script type="text/javascript" src="fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+<script type="text/javascript" src="fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+
+
+
+<!--<script type="text/javascript" src="js/js_redirection_mobile.js"></script>
+-->
+    
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta http-equiv="Content-Style-Type" content="text/css" />
+        <meta name="keywords" content="flv, player, flash, video, generator" />
+        <meta name="author" content="neolao" />
+        <meta name="description" lang="fr" content="Lecteur de fichiers vidéos au format FLV, destiné à la diffusion par internet." />
+        <meta name="description" lang="en" content="Video player for FLV file, intended for the diffusion on websites." />
+        
+        <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="print.css" media="print" />
+        <meta name="verify-v1" content="D3yzpd4WjtC849bV5XOLJVDLtpsyHyQawW13jnxkO10=" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+        <title>
+        FLV Player - Players - MAXI - Preview        </title>
+    </head>
+    <body>
+        <div id="header">
+            <div id="access">
+                <ul>
+                    <li><a href="/accessibility/" accesskey="0" title="Accessibility policy">accessibility</a></li>
+                    <li><a href="#body" accesskey="s" title="Go to the content">content</a></li>
+                    <li><a href="/sitemap/" accesskey="3" title="Go to the site map">site map</a></li>
+                    <li><a href="/contact/" accesskey="7" title="Go to the contact page">contact</a></li>
+                </ul>
+            </div>
+            <h1><img src="/style/images/title_en.png" alt="FLV Player"/></h1>
+        </div>
+        <div id="nav">
+            <ul>
+                <li class="home"><a accesskey="1" href="/">Home</a></li>
+                <li class="players selected"><a href="/players/">Players</a></li>
+                <li class="help"><a href="/help/">Help</a></li>
+            </ul>
+        </div>
+        <div id="subnav">
+            <p>Type: </p>
+            <ul>
+                <li><a href="/players/mini/" >MINI</a></li>
+                <li><a href="/players/normal/" >NORMAL</a></li>
+                <li><a href="/players/maxi/" class="selected">MAXI</a></li>
+                <li><a href="/players/multi/" >MULTI</a></li>
+                <li><a href="/players/js/" >JS</a></li>
+            </ul>
+        </div>
+        <div id="body">
+            <div id="sidebar">
+                <ul>
+                    <li><a href="/players/maxi/preview/"  class="selected">Preview</a></li>
+                    <li><a href="/players/maxi/documentation/"  >Documentation</a></li>
+                    <li><a href="/players/maxi/generator/"  >Generator</a></li>
+                    <li><a href="/players/maxi/download/"  >Download</a></li>
+                    <li><a href="/players/maxi/plugins/"  >Plugins</a></li>
+                    <li><a href="/players/maxi/license/"  >License</a></li>
+                </ul>
+            </div>
+            <h2>Preview</h2>
+			
+			<!--
+            <object class="playerpreview" type="application/x-shockwave-flash" data="player_flv_maxi.swf" width="640" height="400">
+                <param name="movie" value="player_flv_maxi.swf" />
+                <param name="allowFullScreen" value="true" />
+                <param name="FlashVars" value="flv=videos/angels-and-airwaves_anxiety_LG.flv&amp;width=640&amp;height=400&amp;showstop=1&amp;showvolume=1&amp;showtime=1&amp;startimage=videos/angels-and-airwaves_anxiety_LG.jpg&amp;showfullscreen=1&amp;bgcolor1=1A1718&amp;bgcolor2=1A1718&amp;playercolor=1A1718" />
+            </object>
+			-->
+			
+			
+            <object class="playerpreview" type="application/x-shockwave-flash" data="player_flv_maxi.swf" width="640" height="400">
+                <param name="movie" value="player_flv_maxi.swf" />
+                <param name="allowFullScreen" value="true" />
+                <param name="FlashVars" value="flv=videos/angels-and-airwaves_anxiety_LG.flv&amp;width=640&amp;height=400&amp;showstop=1&amp;showvolume=1&amp;showtime=1&amp;startimage=videos/angels-and-airwaves_anxiety_LG.jpg&amp;showfullscreen=1&amp;bgcolor1=1A1718&amp;bgcolor2=1A1718&amp;playercolor=1A1718" />
+            </object>
+			
+			
+            <p>This is the most complete player in terms of functionalities.</p><p>This is the only player that will be improved with new features.</p>            <div id="bodyfooter"></div>
+<div class="pub">
+<h2>Pub</h2>
+            <!--
+    <ul>
+        <li><a href="http://textilewiki.com">Textile Wiki</a></li>
+    </ul>
+    -->
+</div>
+
+        </div>
+        <div id="footer">
+            <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+            <p class="infos">
+                <a class="neolao" href="http://www.neolao.com">neolao production</a> | 
+                <a href="/credits/">Credits</a> |
+                <input type="hidden" name="cmd" value="_s-xclick" />
+                <input type="submit" name="submit" value="Make a donation" />
+                <img alt="" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1" />
+                <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHPwYJKoZIhvcNAQcEoIIHMDCCBywCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYA/vTu/z8iotqQIM5i45tzD0XsA8TH3j8QpNbSdFzUnzZpQKe8fe7YZZXQ2RgZX49fT+W18N9ioFKu78fhRHFn60O8G/S5DpP79oIMg6FQEsM2CcRZmwyaPJzKVs/+AW9LW8mX+O8A7yzbpVF9r7qD31fvMFaThUls3lPPsjKgd+jELMAkGBSsOAwIaBQAwgbwGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIEc9ZHTn7o82AgZjg3whple1jper6O9/6WVyvB5Drvl4dcvG/1TnQBGVxN64pBK9cPlyBZ4RwukZBxkJmCDxbr3+ltBQlBWw0smS7gh+9FRq9kVexY9j3xmYcVhz3gqOuwzHounogi8kWZr7AKk/tdOFrRF3yJGRObQqlqZ6xmG27E6EREMnyYH9Z2MowPRZDh7pR+rrSVRrbl9qu8LeQakaJCKCCA4cwggODMIIC7KADAgECAgEAMA0GCSqGSIb3DQEBBQUAMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTAeFw0wNDAyMTMxMDEzMTVaFw0zNTAyMTMxMDEzMTVaMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbTCBnzANBgkqhkiG9w0BAQEFAAOBjQAwgYkCgYEAwUdO3fxEzEtcnI7ZKZL412XvZPugoni7i7D7prCe0AtaHTc97CYgm7NsAtJyxNLixmhLV8pyIEaiHXWAh8fPKW+R017+EmXrr9EaquPmsVvTywAAE1PMNOKqo2kl4Gxiz9zZqIajOm1fZGWcGS0f5JQ2kBqNbvbg2/Za+GJ/qwUCAwEAAaOB7jCB6zAdBgNVHQ4EFgQUlp98u8ZvF71ZP1LXChvsENZklGswgbsGA1UdIwSBszCBsIAUlp98u8ZvF71ZP1LXChvsENZklGuhgZSkgZEwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tggEAMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQEFBQADgYEAgV86VpqAWuXvX6Oro4qJ1tYVIT5DgWpE692Ag422H7yRIr/9j/iKG4Thia/Oflx4TdL+IFJBAyPK9v6zZNZtBgPBynXb048hsP16l2vi0k5Q2JKiPDsEfBhGI+HnxLXEaUWAcVfCsQFvd2A1sxRr67ip5y2wwBelUecP3AjJ+YcxggGaMIIBlgIBATCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwCQYFKw4DAhoFAKBdMBgGCSqGSIb3DQEJAzELBgkqhkiG9w0BBwEwHAYJKoZIhvcNAQkFMQ8XDTA3MDgxNTIzMjcwMFowIwYJKoZIhvcNAQkEMRYEFNGwlCLGSp4S3nxEsFJm/G9KH4saMA0GCSqGSIb3DQEBAQUABIGAMUhU6tU2ACb7gBvkKwr0kuyvd5xgDHAZIkaaN8upeMT7wWo7/AX8JZcccJHINKxgXaIHMR4C8UuLvgDi/SSUXcRQREvPX+HA/e10Jvb6p6ibKD7q8ntgnGPxqSaRKfzEBo3ECYmt8kkTA0h+mSgri0DfqDrt9RRWhidNQdmqPrU=-----END PKCS7-----" />
+            </p>
+            <p>
+                <a href="/fr/players/maxi/preview/">Version Française</a>            </p>
+            </form>
+        </div>
+        <script type="text/javascript">
+        var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+        document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+        </script>
+        <script type="text/javascript">
+        var pageTracker = _gat._getTracker("UA-2258619-4");
+        pageTracker._initData();
+        pageTracker._trackPageview();
+        </script>
+    </body>
+</html>
