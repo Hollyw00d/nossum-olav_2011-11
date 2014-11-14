e527078190ade3e78c4f85032de5667ab65debfc
Matt Jennings <matt@mattjennings.net>
2014-11-13 16:13:40 -0800

    project done

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) ob_start("ob_gzhandler"); else ob_start(); ?>
-<!DOCTYPE html> 
+
+
+<!DOCTYPE html>
+
 <html>
 <head>
-    <meta charset="utf-8" />
-    <title>Video and Motion Design by Olav Nossum - Seattle WA</title>
-	<script type="text/javascript" src="mediaelement/jquery.js"></script>
-	<script type="text/javascript" src="mediaelement/mediaelement-and-player.min.js"></script>
-	<link rel="stylesheet" href="mediaelement/mediaelementplayer.min.css" />
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />    <title>Video and Motion Design by Olav Nossum - Seattle WA</title>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js" type="text/javascript"></script>
 
-	
     <link rel="stylesheet" href="css/coda-slider-2.0.css" type="text/css" media="screen" />
     <!--[if IE 7]>
     <link rel="stylesheet" href="css/coda-slider-2.0_ie7_min.css" type="text/css" media="screen" />
@@ -27,139 +26,102 @@
         
             </div><!-- #header-wrapper -->
         </div><!-- #header-container -->
-<noscript>
-	<div>
-        <p>Unfortunately your browser does not hava JavaScript capabilities which are required to exploit full functionality of our site. This could be the result of two possible scenarios:</p>
-        <ol>
-            <li>You are using an old web browser, in which case you should upgrade it to a newer version. We recommend the latest version of <a href="http://www.getfirefox.com">Firefox</a>.</li>
-            <li>You have disabled JavaScript in you browser, in which case you will have to enable it to properly use our site. <a href="http://www.google.com/support/bin/answer.py?answer=23852">Learn how to enable JavaScript</a>.</li>
-        </ol>
-    </div>
-</noscript>
 
 <div class="coda-slider-wrapper">
 	<div class="coda-slider preload" id="coda-slider-1">
     
 		<div class="panel">
 			<div class="panel-wrapper">
+
+
+        <?php include("includes/browser-upgrade-warning.php"); ?>
+
 				<h1>Make your vision, visual... with video and motion design.</h1>
                 
                 <h2>Motion Design Reel</h2>
                 
                 <p>
-                
-<!--                  <iframe src="http://player.vimeo.com/video/35923285?title=0&amp;byline=0&amp;portrait=0" width="650" height="366" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
--->
-<!--
-                  <iframe src="video01-LG.php" width="680" height="800" allowtransparency="true" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
--->
 
-<video width="640" height="360" id="player2" poster="videos/reel_2011.png" controls="controls" preload="auto">
+
+<video width="640" height="360" id="player2" poster="videos/reel_2011_LG.png" controls="controls" preload="auto">
 	<!-- MP4 source must come first for iOS -->
 	<source type="video/mp4" src="videos/reel_2011.mp4" />
 	<!-- WebM for Firefox 4 and Opera -->
 	<source type="video/webm" src="videos/reel_2011.webm" />
 	<!-- OGG for Firefox 3 -->
 	<source type="video/ogg" src="videos/reel_2011.ogv" />
-	<!-- Fallback flash player for no-HTML5 browsers with JavaScript turned off -->
-	<object width="640" height="360" type="application/x-shockwave-flash" data="mediaelement/flashmediaelement.swf">
-		<param name="movie" value="mediaelement/flashmediaelement.swf" />
-		<param name="flashvars" value="controls=true&amp;file=videos/reel_2011.mp4" />
-		<!-- Image fall back for non-HTML5 browser with JavaScript turned off and no Flash player installed -->
-		<img src="videos/reel_2011.png" width="640" height="360" alt="Here we are"
-			title="No video playback capabilities" />
-	</object>
 </video>
-
-
-        
-
-
-       
-                    
-                    
-                    
                 </p>
                 
-                <p>
-                Blah blah blah more text hereblah blah blah more text hereblah blah blah more text hereblah blah blah more text hereblah blah blah more text hereblah blah blah more text hereblah blah blah more text here
-                </p>                         
-                
+
 			</div>
 		</div>
 		<div class="panel">
 			<div class="panel-wrapper">
+
+        <?php include("includes/browser-upgrade-warning.php"); ?>
+
             
 				<h1>View a few of my videos and motion design works.</h1>
-                
-                <table>
-                
-                
-                	<tr>
-                    	<td>
-                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
-                     	</td>
-                        <td>
-                        <h2>Larry Bernandez Bobblehead Commercial</h2>
-                        <p>Larry Bernandez Bobblehead Commercial</p>
-                        </td>
-                    </tr>
-                	<tr>
-                    	<td>
-                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
-                     	</td>
-                        <td>
-                        <h2>Larry Bernandez Bobblehead Commercial</h2>
-                        <p>Larry Bernandez Bobblehead Commercial</p>
-                        </td>
-                    </tr>
-                	<tr>
-                    	<td>
-                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
-                     	</td>
-                        <td>
-                        <h2>Larry Bernandez Bobblehead Commercial</h2>
-                        <p>Larry Bernandez Bobblehead Commercial</p>
-                        </td>
-                    </tr>
-                	<tr>
-                    	<td>
-                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
-                     	</td>
-                        <td>
-                        <h2>Larry Bernandez Bobblehead Commercial</h2>
-                        <p>Larry Bernandez Bobblehead Commercial</p>
-                        </td>
-                    </tr>
-                	<tr>
-                    	<td>
-                        <a class="motion-design-reel" href="video01-LG.php"><img src="images/videos/video-thumb-dummy.png" width="209" height="127" alt="video thumb dummy" /></a>
-                     	</td>
-                        <td>
-                        <h2>Larry Bernandez Bobblehead Commercial</h2>
-                        <p>Larry Bernandez Bobblehead Commercial</p>
-                        </td>
-                    </tr>
-                    
-                   
-                </table>
-                
+
+        <table>
+
+
+          <tbody><tr>
+            <td>
+              <a href="videos/bernandez_bobblehead_spot.php" class="motion-design-reel"><img width="203" height="127" alt="Larry Bernandez Bobblehead Spot" src="videos/bernandez_bobblehead_spot.png" /></a>
+            </td>
+            <td>
+              <h2>Larry Bernandez Bobblehead Spot</h2>
+              <p>Shot on Canon 5D Mark II</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <a href="videos/moose_office.php" class="motion-design-reel"><img width="203" height="127" alt="Spoof of the TV show staring the Mariner Moose" src="videos/moose_office.png" /></a>
+            </td>
+            <td>
+              <h2>Moose Office</h2>
+              <p>Spoof of the TV show staring the Mariner Moose</p>
+            </td></tr>
+          <tr>
+            <td>
+              <a href="videos/rally_clip_rob_dyrdek.php" class="motion-design-reel"><img width="203" height="127" alt="Rob Dyrdek Rally Message" src="videos/rally_clip_rob_dyrdek.png" /></a>
+            </td>
+            <td>
+              <h2>Rob Dyrdek Rally Message</h2>
+              <p>C4D, After Effects</p>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <a href="videos/tips.php" class="motion-design-reel"><img width="203" height="127" alt="Safeco Field Tips" src="videos/tips.png" /></a>
+            </td>
+            <td>
+              <h2>Safeco Field Tips</h2>
+              <p>After Effects 2.5D Animation</p>
+            </td>
+          </tr>
+
+
+          </tbody></table>
                 
 			</div>
 		</div>
 		<div class="panel">
 			<div class="panel-wrapper">
-            
-				<h1><span class="big-text">Hi, I’m Olav,</span> blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah. </h1>
+
+        <h1><span class="big-text">Hi, I'm Olav,</span> <br /> I am a motion designer and video producer in Seattle, WA. I love to collaborate and create visual content.</h1>
+
+        <p>
+          <br />
+          ...and a few tech tools I use to make it happen:  Maxon Cinema 4D, Adobe After Effects, Photoshop, Illustrator, Premiere Pro, Apple Final Cut Pro, Microsoft PowerPoint, Excel, MySQL, HTML5, CSS, jQuery.
+        </p>
                 
                 <p class="pic">
                 <img src="images/about/olav-at-puget-sound.png" width="682" height="258" alt="Olav at Puget Sound" />
                 </p>
-                
-				<p>                
-				I live in Seattle, Washington.  blah blah blah I live in Seattle, WA.  blah blah blah I live in Seattle, Washington.  blah blah blah I live in Seattle, Washington.  Blah blah blah.
-				</p>
-                                
+
                 <p class="pic">
                 <img src="images/about/seattle01.png" width="682" height="258" alt="Seattle" />
                 </p>
@@ -170,34 +132,12 @@
 			<div class="panel-wrapper">
 				<h1>Olav Nossum</h1>
                 <h2>
-                <a href="mailto:Olav@CutMotion.com">Olav@CutMotion.com</a>
-                <!--
-                <script type="text/javascript">
-					var addressPart1 = 'Olav@cut'
-					var addressPart2 = 'Motion.c';
-					var addressPart3 = 'om';
-					var emailPart1 = '<a href="';
-					var emailPart2 = 'mailt';
-					var emailPart3 = 'o:';
-					var emailPart4 = '"';
-					var emailPart5 = '>';
-					var emailPart6 = '<';
-					var emailPart7 = '/';
-					var emailPart8 = 'a';
-					var emailPart9 = '>';
-					document.write(emailPart1 + emailPart2 + emailPart3 + addressPart1 + addressPart2 + addressPart3 + emailPart4 + emailPart5 + addressPart1 + addressPart2 + addressPart3 + emailPart6 + emailPart7 + emailPart8 + emailPart9);		
-					</script>
-                    <noscript><span class="red">
-                    If you cannot see my email address on this page you do not have JavaScript enabled.</span> <a href="http://www.alanwood.net/demos/enabling-javascript.html" target="_blank">To enable JavaScript (a common, safe programming language for web sites) and see my email address follow these steps.</a>
-                    </noscript>
-               	-->
+                <a class="underline" href="#contact">&#79;l&#97;&#118;&#64;CutMotio&#110;&#x2E;&#99;&#111;&#109;</a>
                 </h2>
                 
                 <p class="pic">
                 <img src="images/contact/olav-at-mountains.png" width="682" height="258" alt="Olav at the Mountains" />
                 </p>
-                
-				<p>Contact me... Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, eblah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  Hi, I’m Olav, blah, blah, blah.  </p>
 			</div>
 		</div>
         
@@ -223,10 +163,5 @@
 <script type="text/javascript" src="fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
 <script type="text/javascript" src="fancybox/jquery.fancybox-1.3.4.pack.js"></script>
 
-
-
-<!--<script type="text/javascript" src="js/js_redirection_mobile.js"></script>
--->
-    
 </body>
 </html>
