74a4c5e18a5a79a78a19cb6c6346112afaef1d6b
Matt Jennings <matt@mattjennings.net>
2015-02-09 15:29:49 -0800

    updated css to enable h1 on contact page is not cut off

--- includes/browser-upgrade-warning.php
+++ includes/browser-upgrade-warning.php
@@ -3,8 +3,6 @@
   does not
   support these
   videos. Please
-  download <a href="http://www.getfirefox.com" target="_blank" class="underline">Mozilla Firefox</a> or <a href="https://www.google.com/chrome/browser/" target="_blank" class="underline">Google Chrome</a> to
-  view them
-  .</strong>
+  download <a href="http://www.getfirefox.com" target="_blank">Mozilla Firefox</a> or <a href="https://www.google.com/chrome/browser/" target="_blank">Google Chrome</a> to view them.</strong>
 </p>
 <![endif]-->
